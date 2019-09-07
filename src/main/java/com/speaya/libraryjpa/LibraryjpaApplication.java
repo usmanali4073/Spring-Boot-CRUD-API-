e745e567e5274156b9bf67074d1ef8ba464a32f8
Usman <usmanali4073@gmail.com>
2019-09-07 15:36:11 -0400

    Spring Boot CRUD API (MYSQL)

---
+++ src/main/java/com/speaya/libraryjpa/LibraryjpaApplication.java
@@ -0,0 +1,13 @@
+package com.speaya.libraryjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryjpaApplication.class, args);
+	}
+
+}
